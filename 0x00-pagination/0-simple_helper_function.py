4d411eba9c160f272e9c20ca19daf366de1027dd
IgbeAjaga <ajagaigbe144@gmail.com>
2024-01-01 16:26:07 +0000

    Pycodestyle

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -3,6 +3,7 @@
 
 from typing import Tuple
 
+
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """Returns start and end indexes for pagination"""
     start = (page - 1) * page_size
