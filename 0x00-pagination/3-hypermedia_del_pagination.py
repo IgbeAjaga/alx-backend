315e31f57b2f074a888b541d0ff1e0de8f52581d
IgbeAjaga <ajagaigbe144@gmail.com>
2024-01-01 22:36:38 +0000

    pycodestyle

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -1,57 +1,69 @@
 #!/usr/bin/env python3
-"""Deletion-resilient hypermedia pagination"""
+"""
+Deletion-resilient hypermedia pagination
+"""
+
 import csv
 from typing import Dict, List
 
-
 class Server:
-    """Server class to paginate a database of popular baby names"""
+    """Server class to paginate a database of popular baby names.
+    """
+
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
-        """Initializes a new Server instance"""
-        self._cached_dataset = None
-        self._indexed_dataset = None
+        """Initializes a new Server instance
+        """
+        self.__dataset = None
+        self.__indexed_dataset = None
 
-    def _load_dataset(self) -> List[List]:
-        """Loads dataset from file"""
-        if self._cached_dataset is None:
-            with open(self.DATA_FILE, newline='') as file:
-                reader = csv.reader(file)
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
                 dataset = [row for row in reader]
-            self._cached_dataset = dataset[1:]
-        return self._cached_dataset
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
 
     def indexed_dataset(self) -> Dict[int, List]:
-        """Returns a dataset indexed by sorting position, starting at 0"""
-        if self._indexed_dataset is None:
-            dataset = self._load_dataset()
-            truncated_dataset = dataset[:1000]
-            self._indexed_dataset = {
-                idx: item for idx, item in enumerate(dataset)
-            }
-        return self._indexed_dataset
+        """Dataset indexed by sorting position, starting at 0
+        """
+        if self.__indexed_dataset is None:
+            dataset = self.dataset()
+            self.__indexed_dataset = {i: dataset[i] for i in range(len(dataset))}
+        return self.__indexed_dataset
 
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
-        """Returns paginated data with deletion-resilient metadata"""
+        """Returns paginated data with deletion-resilient metadata
+        """
         data = self.indexed_dataset()
-        assert index is not None and 0 <= index <= max(data.keys())
-        paginated_data = []
+        assert index is None or (isinstance(index, int) and index >= 0)
+        assert isinstance(page_size, int) and page_size > 0
+
+        if index is None:
+            index = 0
+
+        page_data = []
         data_count = 0
         next_index = None
-        start = index if index else 0
-        for idx, item in data.items():
-            if idx >= start and data_count < page_size:
-                paginated_data.append(item)
-                data_count += 1
-                continue
-            if data_count == page_size:
-                next_index = idx
+
+        for i in range(index, len(data)):
+            if data_count < page_size:
+                if i in data:
+                    page_data.append(data[i])
+                    data_count += 1
+            else:
+                next_index = i
                 break
+
         page_info = {
             'index': index,
             'next_index': next_index,
-            'page_size': len(paginated_data),
-            'data': paginated_data,
+            'page_size': len(page_data),
+            'data': page_data,
         }
         return page_info
