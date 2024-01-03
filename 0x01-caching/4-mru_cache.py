4f7800151700db5e43268095a8efb3b790326e00
IgbeAjaga <ajagaigbe144@gmail.com>
2024-01-03 19:54:17 +0000

    MRU Caching

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,27 +1,39 @@
 #!/usr/bin/env python3
-""" 4-mru_cache """
+"""4-mru_cache"""
+from collections import OrderedDict
+
 from base_caching import BaseCaching
 
 
 class MRUCache(BaseCaching):
-    """ MRUCache class """
-
+    """MRUCache class that represents an object
+    that allows storing and
+    retrieving items from a dictionary with an MRU
+    removal mechanism when the limit is reached.
+    """
     def __init__(self):
+        """Initializes the cache.
+        """
         super().__init__()
+        self.cache_data = OrderedDict()
 
     def put(self, key, item):
-        """ Put method """
-        if key is not None and item is not None:
-            if len(self.cache_data) >= self.MAX_ITEMS:
-                removed = list(self.cache_data.keys())[0]
-                del self.cache_data[removed]
-                print(f"DISCARD: {removed}")
+        """Adds an item in the cache.
+        """
+        if key is None or item is None:
+            return
+        if key not in self.cache_data:
+            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                mru_key, _ = self.cache_data.popitem(False)
+                print("DISCARD:", mru_key)
+            self.cache_data[key] = item
+            self.cache_data.move_to_end(key, last=False)
+        else:
             self.cache_data[key] = item
 
     def get(self, key):
-        """ Get method """
-        if key in self.cache_data:
-            del self.cache_data[key]
-            self.cache_data[key] = key
-            return self.cache_data[key]
-        return None
+        """Retrieves an item by key.
+        """
+        if key is not None and key in self.cache_data:
+            self.cache_data.move_to_end(key, last=False)
+        return self.cache_data.get(key, None)
