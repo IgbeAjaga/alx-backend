d27f50d4d82e059a4d86f270cae75f95bf0fa6f1
IgbeAjaga <ajagaigbe144@gmail.com>
2024-01-09 22:45:56 +0000

    templates

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """Basic Flask app"""
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -7,6 +8,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    """Render index template"""
     return render_template('0-index.html')
 
 
